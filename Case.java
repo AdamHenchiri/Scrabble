b2e317165e4e6600cb8713551441c5576dc4d756
AdamHenchiri <94307418+AdamHenchiri@users.noreply.github.com>
2021-12-10 15:25:31 +0100

    Add files via upload

---
+++ Case.java
@@ -0,0 +1,29 @@
+public class Case{
+    private int couleur;
+    private char letter;
+
+    public Case (int uneCouleur){
+        this.couleur=uneCouleur;
+    }
+
+    public int getCouleur() {
+        return couleur;
+    }
+
+    public char getLetter() {
+        return letter;
+    }
+
+    public void setLetter(char let) {
+        this.letter = let;
+    }
+    public Boolean estRecouvert(){
+        return(this.letter != ' ');
+    }
+
+    @Override
+    public String toString() {
+        return "Case [couleur=" + couleur + ", letter=" + letter + "]";
+    }
+    
+}
