2c9bf04fe2149d05b54c0f387b0d0698b71ffbdc
AdamHenchiri <adamhenchiri02@gmail.com>
2022-01-05 13:06:37 +0100

    MainScrabble

---
+++ MainScrabble.java
@@ -0,0 +1,16 @@
+public class  MainScrabble{
+    public static void main(String[] args) {
+        int nbjoueur=0;
+        do{
+        System.out.println("veuillez saisir le nombre de joueurs...");
+        nbjoueur=Ut.saisirEntier();
+        } while (nbjoueur>=1 && nbjoueur<15);
+        String [] tabNomJ = new String [nbjoueur];
+        for (int i=0;i<tabNomJ.length;i++){
+            System.out.println("veillez saisir le nom du joueur num "+i+1);
+            tabNomJ[i]=Ut.saisirChaine();
+        }
+        new Scrabble (tabNomJ);
+
+    }
+}
