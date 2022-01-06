efebb8b2eac2cee67342b3079e455f7b31d40ad8
Purgator <95345332+Geoffreypierre@users.noreply.github.com>
2022-01-06 22:36:59 +0100

    Update Joueur.java

--- Joueur.java
+++ Joueur.java
@@ -26,10 +26,10 @@ public class Joueur {
     public String toString() {
 
         return "{" +
-                " nom='" + getNom() + "'" +
-                ", chevalet='" + getChevalet() + "'" +
-                ", score='" + getScore() + "'" +
-                "}";
+        " nom='" + getNom() + "'" +
+        ", chevalet='" + getChevalet() + "'" +
+        ", score='" + getScore() + "'" +
+        "}";
     }
 
     public void ajouteScore(int nb) {
@@ -71,8 +71,9 @@ public class Joueur {
             }
         }
          /* le cas qu'il reste est si le nombre de jetons restant dans le sac est inférieur au nombre de jetons que le joueur doit piocher,
-            on transfère donc l'intégralité du sac dans le chevalet du joueur  */
-        else if (s.getNbTotEx()<=nbJetons && s.getNbTotEx()!=0){
+         on transfère donc l'intégralité du sac dans le chevalet du joueur  */
+
+         else if (s.getNbTotEx()<=nbJetons && s.getNbTotEx()!=0){
             for (int i = 0 ; i < s.getTabFreq().length ; i++) {
                 if (s.getEltTabFreq(i)!=0){
                     s.transfere(this.chevalet,i);
@@ -82,56 +83,164 @@ public class Joueur {
             System.out.println("Erreur: sac vide !!");
         }
     }
+
     public int joue(Plateau p, MEE s, int[] nbPointsJet) {
+
+        int resultat = 0;
         char reponse='\0';
-        int resultat=2;
-        int nbJetEchange=0;
-        char lettreChange='\0';
-        String motjoue = "";
-        int numerocol = 0;
-        int numeroli = 0;
-        char sens = 's';
+
         do{
             System.out.println("veillez choisir votre reponse ");
             System.out.println("E pour ECHANGER un ou plusieurs jeton/s..");
             System.out.println("P pour PASSER votre tour..");
             System.out.println("J pour JOUER un mot..");
             reponse=Ut.saisirCaractere();
-        }while(reponse!='E'||reponse!='P'||reponse!='J');
+
+        }while(!(reponse!='E'||reponse!='P'||reponse!='J'));
+
         if (reponse=='E'){
-                do{
-                System.out.println("combien de lettre voulez vous echanger ?");
-                 nbJetEchange=Ut.saisirEntier();
-                }while(nbJetEchange>0 && nbJetEchange <= 7);
-                for (int i=0;i<nbJetEchange;i++){
-                System.out.println("quelle lettre voulez vouz echanger ?");
-                lettreChange=Ut.saisirCaractere();
-                this.chevalet.transfere(s,Ut.majToIndex(lettreChange));
-                }
-                this.prendJetons(s,nbJetEchange);
 
-                resultat=0;
+            resultat = 0;     
         }
-        else if (reponse=='P') {
-                 
-                    resultat = -1;
-                }          
-             
+        else if (reponse=='P') { 
+
+            resultat = -1
+        }          
+
         else if (reponse=='J') {
-                do {
-                System.out.println("saisir le mot que vous souhaitez jouer !");
-                motjoue=Ut.saisirChaine();
-                System.out.println("saisir le numéro de la colonne de votre première lettre !");
-                numerocol = Ut.saisirEntier();
-                System.out.println("saisir le numéro de la ligne de votre première lettre !");
-                numeroli = Ut.saisirEntier();
-                System.out.println("donner le sens de votre mot !");
-                sens = Ut.saisirCaractere();
 
-                while (!(placementValide(motjoue,numeroli,numerocol,sens,this.chevalet)))    
+            resultat = 1;
+        }
+        
 
-
-                }
-        }     
+        return resultat;
     }
+
+    public int joueMot(Plateau p, MEE s, int[] nbPointsJet) {
+
+        String motjoue = "";
+        int numCol = 0;
+        int numLig = 0;
+        char sens = 'sens';
+
+        do {
+            System.out.println("saisir le mot que vous souhaitez jouer !");
+            motjoue=Ut.saisirChaine();
+            System.out.println("saisir le numéro de la colonne de votre première lettre !");
+            numCol = Ut.saisirEntier();
+            System.out.println("saisir le numéro de la ligne de votre première lettre !");
+            numLig = Ut.saisirEntier();
+            System.out.println("donner le sens de votre mot !");
+            sens = Ut.saisirCaractere();
+
+        }while(!(p.placementValide(motjoue,numLig,numCol,sens,s)));
+
+        joueMotAux(p,s,nbPointsJet,motjoue,numLig,numCol,sens);
+
+    }
+
+    public void joueMotAux(Plateau p, MEE s, int[] nbPointsJet, String mot,int numLig, int numCol, char sens) {
+
+        p.place(mot,numLig,numCol,sens,s);
+    }       
+
+
+
+    public boolean estCorrectPourEchange (String mot) {
+
+        boolean res = false;
+
+        int i = 0;
+
+        MEE e1 = new MEE(this.chevalet);
+
+        for (i = 0 ; i < mot.length() ; i++ ) {
+
+            if (estUneMaj(mot.charAt(i))) {
+
+                while (res == false) {
+
+                    res = !(e1.getEltTabFreq(Ut.majToIndex(mot.charAt(i))) > 0);
+                    e1.retire(Ut.majToIndex(mot.charAt(i)));
+                }
+            }
+        }
+        return res;    
+    }
+
+    public void echangeJetonsAux(MEE sac, String ensJetons) {
+
+        int nbJetEchange=0;
+
+        do {
+            System.out.println("combien de lettre voulez vous echanger ?");
+
+            nbJetEchange = Ut.saisirEntier();
+
+        }while(nbJetEchange <1 && nbJetEchange > 7);
+
+        this.prendJetons(sac,nbJetEchange);
+    
+        for (int i=0;i<ensJetons.length();i++){
+           
+            this.chevalet.transfere(sac,Ut.majToIndex(ensJetons.charAt(i)));
+        }
+    }
+
+    public void echangeJetons(MEE sac) {
+
+        String ensJetons = "";
+
+        System.out.println("quel mot voulez vous echanger ?");
+
+        ensJetons = Ut.saisirChaine();
+
+        if (estCorrectPourEchange(ensJetons)) {
+
+            echangeJetonsAux(sac,ensJetons);
+
+        }
+    }    
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
